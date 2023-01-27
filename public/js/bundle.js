4651432512789fe242518d2577d0768c22569954
Jamar Andrade <jandrade2018@fau.edu>
2023-01-26 21:23:14 -0500

    deleting cache

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12387,7 +12387,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60115" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56351" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
